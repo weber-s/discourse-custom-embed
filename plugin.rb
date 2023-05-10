0ba99b98095008fd55f5ab30d3ee60dd6a76939c
Sharif Nada <s.nada@uq.edu.au>
2023-05-10 14:24:53 +1000

    initial commit

---
+++ plugin.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# name: discourse-custom-embed
+# about: A simple plugin to used to show real names in Discourse embeds
+# version: 0.0.1
+# authors: Sharif Nada
+# url: https://github.com/ATB-UQ/discourse-custom-embed
+# required_version: 3.1.0
+
+after_initialize do
+    ::EmbedController.prepend_view_path File.expand_path("../custom_views", __FILE__)
+  end
